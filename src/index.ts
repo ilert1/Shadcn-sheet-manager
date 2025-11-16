00365af17d1008d54b8966b3e6cedfba18249b1e
David <=>
2025-11-16 18:17:16 +0100

    fix: Fixed build nad export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export * from "./SheetManager";
+export { createSheetManager } from "./SheetManager";
+export type {
+  BaseSheetProps,
+  SheetData,
+  SheetState,
+  SheetManagerConfig,
+} from "./types";
